6d907aaff898d18334da10073ff4c0f8ce7b5dbd
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-19 16:27:40 +0200

    added scroll

--- src/js/educationBtn.js
+++ src/js/educationBtn.js
@@ -3,4 +3,8 @@ const show = document.querySelector('.education__title__btn');
 
 show.addEventListener('click', () => {
   content.classList.toggle('show-content');
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: 'smooth',
+  });
 });
