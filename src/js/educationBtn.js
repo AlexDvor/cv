e7cef58f0e13f7afc797f47b955661104f3fc1ca
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-28 23:06:03 +0200

    added onClickButton + icon

--- src/js/educationBtn.js
+++ src/js/educationBtn.js
@@ -1,10 +1,16 @@
 const content = document.getElementById('content-list');
-const show = document.querySelector('.education__title__btn');
+const btnRef = document.querySelector('.education__title__btn');
+const arrowBtnRef = document.querySelector('.arrow-icon');
 
-show.addEventListener('click', () => {
+btnRef.addEventListener('click', onClickButton);
+arrowBtnRef.addEventListener('click', onClickButton);
+
+function onClickButton() {
   content.classList.toggle('show-content');
+  arrowBtnRef.classList.toggle('open');
+
   window.scrollTo({
     top: document.documentElement.scrollHeight,
     behavior: 'smooth',
   });
-});
+}
